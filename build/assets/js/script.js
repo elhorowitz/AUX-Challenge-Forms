2f6bb9fac1c513391d3a616816cfc4966af659f1
elhorowitz <ericalhorowitz@gmail.com>
2014-04-02 21:32:43 -0400

    cleaner js - part II

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -5,25 +5,25 @@
 		amex : {
 			radiobutton : '#amex-cc',
 			cc_regex : /^3[47]/,
-			cc_length : 15,
+			cc_length : '[0-9]{15}',
 			cvv_regex : '[0-9]{4}'
 		},
 		visa : {
 			radiobutton : '#visa-cc',
 			cc_regex : /^4/,
-			cc_length : [13,16],
+			cc_length : '[0-9]{13,16}',
 			cvv_regex : '[0-9]{3}'
 		},
 		discover : {
 			radiobutton : '#discover-cc',
 			cc_regex : /^6(?:011|5|4[4-9]|22(?:1(?:2[6-9]|[3-9]\d)|[2-8]\d{2}|9(?:[01]\d|2[0-5])))/,
-			cc_length : 16,
+			cc_length : '[0-9]{16}',
 			cvv_regex : '[0-9]{3}'
 		},
 		mastercard : {
 			radiobutton : '#mastercard-cc',
 			cc_regex : /^5[0-5]/,
-			cc_length : 16,
+			cc_length : '[0-9]{16}',
 			cvv_regex : '[0-9]{3}'
 		}
 	};
@@ -38,101 +38,48 @@
 		$.each(cardDetails, function(card, details) {
 			//console.log(details.cc_regex+" "+cardnum);
 			if (details.cc_regex.test(cardnum)){
-				console.log(card);
-				changeCard(card);
-			}
+				//console.log(card);
+				type = card;
+			};
 		});
 
-		// if (cardnum.substr(0,1).is(cardDetails.)) {
-		// 	//console.log("visa");
-		// 	type = 'visa';
-		// } else if (cardnum.substr(0,2) == 34 || cardnum.substr(0,2) == 37) {
-		// 	//console.log("amex");
-		// 	type = 'amex';
-		// } else if (cardnum.substr(0,2) >= 50 && cardnum.substr(0,2) <= 55) {
-		// 	//console.log(cardnum.substr(0,2));
-		// 	type = 'mastercard';
-		// } else if (cardnum.substr(0,2) == 65 || (cardnum.substr(0,3) >= 644 && cardnum.substr(0,3) <= 649) || cardnum.substr(0,4) == 6011 || (cardnum.substr(0,6) >= 622126 && cardnum.substr(0,6) <= 622925)) {
-		// 	//console.log("discover");
-		// 	type = 'discover';
-		// }
-
-		// return type;
+		//console.log(type);
+		changeCard(type);
 	}
+
 
 	/*-----changes html/css based on card type-----*/
 	function changeCard(type){
+
 		if(type){
-			switch(type) {
-				case 'amex':
-					//console.log("amex");
-					$('#amex-cc').prop('disabled', false).prop('checked', true);
-					$('#visa-cc').prop('disabled', true).prop('checked', false);
-					$('#discover-cc').prop('disabled', true).prop('checked', false);	
-					$('#mastercard-cc').prop('disabled', true).prop('checked', false);
 
+			var radio = cardDetails[type].radiobutton;
+			var length = cardDetails[type].cc_length;
+			var cvv_loc = cardDetails[type].cvv_type;
+			var cvv_length = cardDetails[type].cvv_regex;
 
-					$('#amex_scode_loc').css('display', 'block');
-					$('#other_scode_loc').css('display', 'none');
-					$('#cardnum').prop('pattern', '[0-9]{15}');
-					$('#scode').prop('pattern', '[0-9]{4}');
-					$('#scode').prop('title', 'Example: 1234');
-					break;
+			$(radio).prop('disabled', false).prop('checked', true);
+			$('#cardnum').prop('pattern', length);
+			$('#scode').prop('pattern', cvv_length);
 
-				case 'visa':
-					//console.log("visa");
-					$('#amex-cc').prop('disabled', true).prop('checked', false);
-					$('#visa-cc').prop('disabled', false).prop('checked', true);
-					$('#discover-cc').prop('disabled', true).prop('checked', false);	
-					$('#mastercard-cc').prop('disabled', true).prop('checked', false);
-					
-					$('#amex_scode_loc').css('display', 'none');
-					$('#other_scode_loc').css('display', 'block');
-					$('#cardnum').prop('pattern', '[0-9]{13|16}');
-					$('#scode').prop('pattern', '[0-9]{3}');
-					$('#scode').prop('title', 'Example: 123');
-					break;
+			if(type == 'amex'){
+				$('#amex_scode_loc').css('display', 'block');
+				$('#other_scode_loc').css('display', 'none');
+			} else {
+				$('#amex_scode_loc').css('display', 'none');
+				$('#other_scode_loc').css('display', 'block');
+			}
+		} else {
+			$.each(cardDetails, function(card, details) {
+				var radio = details.radiobutton;	
+				$(radio).prop('disabled', true).prop('checked', false);
+			});
 
-				case 'discover':
-					//console.log("discover");
-					$('#amex-cc').prop('disabled', true).prop('checked', false);
-					$('#visa-cc').prop('disabled', true).prop('checked', false);
-					$('#discover-cc').prop('disabled', false).prop('checked', true);	
-					$('#mastercard-cc').prop('disabled', true).prop('checked', false);
-					
-					$('#amex_scode_loc').css('display', 'none');
-					$('#other_scode_loc').css('display', 'block');
-					$('#cardnum').prop('pattern', '[0-9]{16}');
-					$('#scode').prop('pattern', '[0-9]{3}');
-					$('#scode').prop('title', 'Example: 123');
-					break;
-
-				case 'mastercard':
-					//console.log("mastercard");
-					$('#amex-cc').prop('disabled', true).prop('checked', false);
-					$('#visa-cc').prop('disabled', true).prop('checked', false);
-					$('#discover-cc').prop('disabled', true).prop('checked', false);	
-					$('#mastercard-cc').prop('disabled', false).prop('checked', true);
-					
-					$('#amex_scode_loc').css('display', 'none');
-					$('#other_scode_loc').css('display', 'block');
-					$('#cardnum').prop('pattern', '[0-9]{16}');
-					$('#scode').prop('pattern', '[0-9]{3}');
-					$('#scode').prop('title', 'Example: 123');
-					break;
-
-				default:
-					$('#amex-cc').prop('disabled', true).prop('checked', false);
-					$('#visa-cc').prop('disabled', true).prop('checked', false);
-					$('#discover-cc').prop('disabled', true).prop('checked', false);	
-					$('#mastercard-cc').prop('disabled', true).prop('checked', false);
-					
-					$('#cardnum').prop('pattern', '[0-9]{13, 16}');
-					$('#scode').prop('pattern', '[0-9]{3,4}');
-					$('#scode').prop('title', 'Example: 123');
-				break;
-			};
-		};
+			$('#cardnum').prop('pattern', '[0-9]{13,16}');
+			$('#scode').prop('pattern', '[0-9]{3,4}');
+			$('#amex_scode_loc').css('display', 'none');
+			$('#other_scode_loc').css('display', 'block');
+		}
 	}
 
 /*--------------executable code--------------*/
@@ -141,8 +88,7 @@
 
 	$('#cardnum').bind('keyup blur', function() {
 		var cardnum = $('#cardnum').val();
-		var type = checkCardType(cardnum);
-		changeCard(type);
+		checkCardType(cardnum);
 		//console.log($('#cardnum').prop('pattern'));
 	});
 
